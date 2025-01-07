568376181743102aca64997bdeca583f20bfc081
Elham Dolatabadi <elham.dolatabadi@gmail.com>
2025-01-07 11:35:18 -0500

    Update CCHS-data-import.R

--- R_Scripts/CCHS-data-import.R
+++ R_Scripts/CCHS-data-import.R
@@ -1,5 +1,5 @@
 # Specify the file path
-file_path <- "~/Desktop/CCHS/cchs-82M0013-E-2017-2018-Annual-component_F1-subset.tab"
+file_path <- "~/Desktop/CCHS/cchs-82M0013-E-2015-2016-Annual-component_F1-subset.tab"
 
 if (file.exists(file_path)) {
   data <- read.table(file_path, header = TRUE, sep = "\t")
